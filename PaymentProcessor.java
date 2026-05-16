e8fc8d2ffb65a51a81590d393551dbb5deb111a5
heroxzain <mz924823@gmail.com>
2026-05-16 16:27:39 +0500

    Revert "Added PaymentProcessor with payment logic"
    This reverts commit 4fc0498e485e15097401bb865ff52c4156c6dd0b.

--- PaymentProcessor.java
+++
@@ -1,8 +0,0 @@
-public class PaymentProcessor {
-
-    public void processPayment() {
-        int amount = 100;
-        int result = amount / 0; // Critical bug
-        System.out.println(result);
-    }
-}
